6761a166196429d677fd1769481b4a30813178d7
Fer <fer@fers-mac.local>
2016-06-03 00:41:23 -0500

    Final Release

--- Lab03/Start.h
+++ Lab03/Start.h
@@ -10,6 +10,82 @@
 
 @interface Start : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIScrollView *scroll_view;
+//------------------Boxer 1----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive1;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead1;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer1;
+- (IBAction)btnAlive1Pressed:(UIButton *)sender;
+- (IBAction)btnDead1Pressed:(UIButton *)sender;
+
+//------------------Boxer 2----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive2;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead2;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer2;
+- (IBAction)btnAlive2Pressed:(UIButton *)sender;
+- (IBAction)btnDead2Pressed:(UIButton *)sender;
+
+//------------------Boxer 3----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive3;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead3;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer3;
+- (IBAction)btnAlive3Pressed:(UIButton *)sender;
+- (IBAction)btnDead3Pressed:(UIButton *)sender;
+
+//------------------Boxer 4----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive4;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead4;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer4;
+- (IBAction)btnAlive4Pressed:(UIButton *)sender;
+- (IBAction)btnDead4Pressed:(UIButton *)sender;
+
+//------------------Boxer 5----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive5;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead5;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer5;
+- (IBAction)btnAlive5Pressed:(UIButton *)sender;
+- (IBAction)btnDead5Pressed:(UIButton *)sender;
+
+//------------------Boxer 6----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive6;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead6;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer6;
+- (IBAction)btnAlive6Pressed:(UIButton *)sender;
+- (IBAction)btnDead6Pressed:(UIButton *)sender;
+
+//------------------Boxer 7----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive7;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead7;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer7;
+- (IBAction)btnAlive7Pressed:(UIButton *)sender;
+- (IBAction)btnDead7Pressed:(UIButton *)sender;
+
+//------------------Boxer 8----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive8;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead8;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer8;
+- (IBAction)btnAlive8Pressed:(UIButton *)sender;
+- (IBAction)btnDead8Pressed:(UIButton *)sender;
+
+//------------------Boxer 9----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive9;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead9;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer9;
+- (IBAction)btnAlive9Pressed:(UIButton *)sender;
+- (IBAction)btnDead9Pressed:(UIButton *)sender;
+
+//------------------Boxer 10----------------------
+@property (strong, nonatomic) IBOutlet UIButton *btnAlive10;
+@property (strong, nonatomic) IBOutlet UIButton *btnDead10;
+@property (strong, nonatomic) IBOutlet UIImageView *imgAnswer10;
+- (IBAction)btnAlive10Pressed:(UIButton *)sender;
+- (IBAction)btnDead10Pressed:(UIButton *)sender;
+
+//------------------btnNext----------------------
+- (IBAction)btnNextPressed:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UIButton *btnNext;
+@property (strong, nonatomic) IBOutlet UIButton *btnNextGood;
+@property (strong, nonatomic) IBOutlet UIButton *btnNextBad;
 
 @end
 
--- Lab03/Start.m
+++ Lab03/Start.m
@@ -14,8 +14,15 @@
 
 @implementation Start
 
+static int score = 0;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.scroll_view setScrollEnabled:YES];
+    [self.scroll_view setContentSize:CGSizeMake(320,2900)];
+    self.btnNext.hidden = TRUE;
+    self.btnNextGood.hidden = TRUE;
+    self.btnNextBad.hidden = TRUE;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +31,147 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnAlive1Pressed:(UIButton *)sender {
+    self.imgAnswer1.image = [UIImage imageNamed:@"bad.png"];
+    self.btnDead1.enabled = FALSE;
+    self.btnAlive1.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)btnDead1Pressed:(UIButton *)sender {
+    self.imgAnswer1.image = [UIImage imageNamed:@"good.png"];
+    self.btnAlive1.enabled = FALSE;
+    self.btnDead1.backgroundColor = [UIColor redColor];
+    score++;
+}
+
+- (IBAction)btnAlive2Pressed:(UIButton *)sender {
+    self.imgAnswer2.image = [UIImage imageNamed:@"bad.png"];
+    self.btnDead2.enabled = FALSE;
+    self.btnAlive2.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)btnDead2Pressed:(UIButton *)sender {
+    self.imgAnswer2.image = [UIImage imageNamed:@"good.png"];
+    self.btnAlive2.enabled = FALSE;
+    self.btnDead2.backgroundColor = [UIColor redColor];
+    score++;
+}
+- (IBAction)btnAlive3Pressed:(UIButton *)sender {
+    self.imgAnswer3.image = [UIImage imageNamed:@"good.png"];
+    self.btnDead3.enabled = FALSE;
+    self.btnAlive3.backgroundColor = [UIColor redColor];
+    score++;
+}
+
+- (IBAction)btnDead3Pressed:(UIButton *)sender {
+    self.imgAnswer3.image = [UIImage imageNamed:@"bad.png"];
+    self.btnAlive3.enabled = FALSE;
+    self.btnDead3.backgroundColor = [UIColor redColor];
+}
+- (IBAction)btnAlive4Pressed:(UIButton *)sender {
+    self.imgAnswer4.image = [UIImage imageNamed:@"bad.png"];
+    self.btnDead4.enabled = FALSE;
+    self.btnAlive4.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)btnDead4Pressed:(UIButton *)sender {
+    self.imgAnswer4.image = [UIImage imageNamed:@"good.png"];
+    self.btnAlive4.enabled = FALSE;
+    self.btnDead4.backgroundColor = [UIColor redColor];
+    score++;
+}
+- (IBAction)btnAlive5Pressed:(UIButton *)sender {
+    self.imgAnswer5.image = [UIImage imageNamed:@"bad.png"];
+    self.btnDead5.enabled = FALSE;
+    self.btnAlive5.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)btnDead5Pressed:(UIButton *)sender {
+    self.imgAnswer5.image = [UIImage imageNamed:@"good.png"];
+    self.btnAlive5.enabled = FALSE;
+    self.btnDead5.backgroundColor = [UIColor redColor];
+    score++;
+}
+- (IBAction)btnAlive6Pressed:(UIButton *)sender {
+    self.imgAnswer6.image = [UIImage imageNamed:@"good.png"];
+    self.btnDead6.enabled = FALSE;
+    self.btnAlive6.backgroundColor = [UIColor redColor];
+    score++;
+}
+
+- (IBAction)btnDead6Pressed:(UIButton *)sender {
+    self.imgAnswer6.image = [UIImage imageNamed:@"bad.png"];
+    self.btnAlive6.enabled = FALSE;
+    self.btnDead6.backgroundColor = [UIColor redColor];
+}
+- (IBAction)btnAlive7Pressed:(UIButton *)sender {
+    self.imgAnswer7.image = [UIImage imageNamed:@"bad.png"];
+    self.btnDead7.enabled = FALSE;
+    self.btnAlive7.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)btnDead7Pressed:(UIButton *)sender {
+    self.imgAnswer7.image = [UIImage imageNamed:@"good.png"];
+    self.btnAlive7.enabled = FALSE;
+    self.btnDead7.backgroundColor = [UIColor redColor];
+    score++;
+}
+- (IBAction)btnAlive8Pressed:(UIButton *)sender {
+    self.imgAnswer8.image = [UIImage imageNamed:@"good.png"];
+    self.btnDead8.enabled = FALSE;
+    self.btnAlive8.backgroundColor = [UIColor redColor];
+    score++;
+}
+
+- (IBAction)btnDead8Pressed:(UIButton *)sender {
+    self.imgAnswer8.image = [UIImage imageNamed:@"bad.png"];
+    self.btnAlive8.enabled = FALSE;
+    self.btnDead8.backgroundColor = [UIColor redColor];
+}
+- (IBAction)btnAlive9Pressed:(UIButton *)sender {
+    self.imgAnswer9.image = [UIImage imageNamed:@"good.png"];
+    self.btnDead9.enabled = FALSE;
+    self.btnAlive9.backgroundColor = [UIColor redColor];
+    score++;
+}
+
+- (IBAction)btnDead9Pressed:(UIButton *)sender {
+    self.imgAnswer9.image = [UIImage imageNamed:@"bad.png"];
+    self.btnAlive9.enabled = FALSE;
+    self.btnDead9.backgroundColor = [UIColor redColor];
+}
+- (IBAction)btnAlive10Pressed:(UIButton *)sender {
+    self.imgAnswer10.image = [UIImage imageNamed:@"good.png"];
+    self.btnDead10.enabled = FALSE;
+    self.btnAlive10.backgroundColor = [UIColor redColor];
+    score++;
+    if(score == 10)
+    {
+        self.btnNextGood.hidden = FALSE;
+    }
+    else if((score < 10) && (score > 5))
+    {
+        self.btnNext.hidden = FALSE;
+    }
+    else
+    {
+        self.btnNextBad.hidden = FALSE;
+    }
+}
+
+- (IBAction)btnDead10Pressed:(UIButton *)sender {
+    self.imgAnswer10.image = [UIImage imageNamed:@"bad.png"];
+    self.btnAlive10.enabled = FALSE;
+    self.btnDead10.backgroundColor = [UIColor redColor];
+    if((score < 10) && (score > 5))
+    {
+        self.btnNext.hidden = FALSE;
+    }
+    else
+    {
+        self.btnNextBad.hidden = FALSE;
+    }
+}
+- (IBAction)btnNextPressed:(UIButton *)sender {
+}
 @end
